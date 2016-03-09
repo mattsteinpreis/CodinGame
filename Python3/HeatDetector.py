bf291b338863225c4ee572923541ddd138b8877c
mattsteinpreis <matt@nexosis.com>
2016-03-09 16:03:50 -0500

    finished; 100%

--- Python3/HeatDetector.py
+++ Python3/HeatDetector.py
@@ -1,4 +1,44 @@
-__author__ = 'matt'
+import numpy as np
 
-width, height = [int(i) for i in raw_input().split()]
-n_jumps = int(raw_input())
+def update_box(box, pos, direction):
+    x, y = pos
+    xmin, xmax = box[0]
+    ymin, ymax = box[1]
+    if len(direction) == 1:
+        direction += direction
+
+    if direction[0] == 'U':
+        ymax = y-1
+    if direction[0] == 'D':
+        ymin = y+1
+    if direction[1] == 'L':
+        xmax = x-1
+    if direction[1] == 'R':
+        xmin = x+1
+    if direction[1] in ['U', 'D']:
+        xmin = xmax = x
+    if direction[0] in ['L', 'R']:
+        ymin = ymax = y
+
+    return [(xmin, xmax), (ymin, ymax)]
+
+def center(box):
+    xmin, xmax = box[0]
+    ymin, ymax = box[1]
+    x = int(np.mean([xmin, xmax]))
+    y = int(np.mean([ymin, ymax]))
+    return x, y
+
+width, height = [int(i) for i in input().split()]
+n_jumps = int(input())
+x0, y0 = [int(i) for i in input().split()]
+
+current_box = [(0, width), (0, height)]
+current_pos = [x0, y0]
+while True:
+    bomb_dir = input()
+    current_box = update_box(current_box, current_pos, bomb_dir)
+    current_pos = center(current_box)
+    print("{} {}".format(current_pos[0], current_pos[1]))
+
+
