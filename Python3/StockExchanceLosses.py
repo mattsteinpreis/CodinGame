0b70ab130f6af8160f099b106aa553f80d04d9e7
mattsteinpreis <matt@nexosis.com>
2016-03-09 16:36:38 -0500

    finished; 100%

--- Python3/StockExchanceLosses.py
+++ Python3/StockExchanceLosses.py
@@ -1,1 +1,21 @@
 __author__ = 'matt'
+
+# written for Python3
+
+n = int(input())
+stocks = [int(i) for i in input().split()]
+
+left = 0
+right = 1
+max_drop = 0
+while right < n:
+    sr = stocks[right]
+    sl = stocks[left]
+    drop = sr-sl
+    #print(left, right, drop)
+    max_drop = min(max_drop, drop)
+    if sr > sl:
+        left = right
+    right += 1
+max_drop = min(max_drop, 0)
+print(max_drop)
